d1e9b81c011dc749d3f74cfddb90547d76e0849c
Admin <Admin@TEB123004>
2023-09-29 14:39:11 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace uppgift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många dagar vill du hyra bilen?");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine(" Hur många kilometer vill du köra ?");
+            int y = int.Parse(Console.ReadLine());
+            int z = x * 500;
+            int k = 300 + z + y;
+            Console.WriteLine("Den totala suman blir " + k + "kr");
+            Console.ReadKey();
+        }
+    }
+}
